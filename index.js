7343854042bf0815f2dc66550bd38cfe718d483e
ArizaTrizniak <arizatrizniak@gmail.com>
2024-07-13 20:11:20 -0500

    Inline keyboard replaced by persistent keyboard. Removed files for vercel, DB slices, and scheme.

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-
-// SCENES
-require("./bot/middleware/scene/index.scene");
-
-// ON
-
 // COMMANDS
 require("./bot/middleware/command/commands.command");
 require("./bot/middleware/command/start.command");
 
-// HEARS
-require("./bot/middleware/hears/one.hears");
-require("./bot/middleware/hears/two.hears");
-
-// ACTION
-require("./bot/middleware/action/callback.action");
+// ON
+require("./bot/middleware/on/other.on");
 
 // CONNECTION
 require("./bot/connection/local.connection");
